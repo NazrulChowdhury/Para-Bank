efd77f8caada4120bbd6e16dd2f532193f3758cb
Nazrul Chowdhury <nazrulchowdhury404@gmail.com>
2021-05-09 17:03:08 +0100

    login spec added

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -1,5 +1,21 @@
 import user from "../fixtures/userInfo.json"
 
 describe('log in testsuit',()=>{
-    
+    beforeEach(()=>{
+        cy.visit('/')
+        cy.title().should('eq','ParaBank | Welcome | Online Banking')
+    })
+    it('successfully login with correct credentials',()=>{
+        cy.login(user)
+    })
+    it('login attempt fails with wrong username',()=>{
+        cy.submitLogin(user.wrongUsername,user.password)
+        cy.url().should('contain','login')
+        cy.contains('h1','Error!')
+    })  
+    it('login attempt fails with wrong password',()=>{
+        cy.submitLogin(user.userName,user.wrongPassword)
+        cy.url().should('contain','login')
+        cy.contains('h1','Error!')
+    })      
 })
